3dffc18251ae39b4a4a2150c5d0069c85a731e2d
garlicleek <1450509083@qq.com>
2024-03-01 13:59:29 +0800

    识别结果

--- front/uniapp/pages/index/index.vue
+++ front/uniapp/pages/index/index.vue
@@ -14,18 +14,21 @@
 					立即识别多种动物
 			</text>
 		</view>
+		<view class="predict-result" v-show="prediction!==''">
+			<text class="result">识别结果: {{prediction}}</text>
+			<button class="result-btn">查看详情</button>
+		</view>
 		<button class="predict-btn" @click="predict">
 			<image src="/static/icons/camera.png" class="btn-img" mode="aspectFit"></image>
 			拍照识别
 		</button>
-		<view>{{prediction}}</view>
 	</view>
 </template>
 
 <script setup>
 	import {ref} from "vue";
 
-	const backUrl="http://10.10.25.13:8080/predict";
+	const backUrl="http://10.10.25.13:5000/predict";
 	let imageUrl=ref("");
 	let prediction=ref("");
 	
@@ -38,26 +41,32 @@
 				imageUrl.value = res.tempFilePaths[0];
 				console.log("FilePath:"+imageUrl.value);
 				
+				uni.showLoading({
+				        title: '加载中...'
+				});
+				
 				uni.uploadFile({
 					url:backUrl,
 					filePath: imageUrl.value,
 					name:"file",
 					success: (uploadFileRes) => {
-						console.log(JSON.parse(uploadFileRes.data));
 						prediction.value = JSON.parse(uploadFileRes.data).prediction;
-						
+						console.log(prediction.value);
+						uni.hideLoading();
 					},
 					fail: (uploadFileErr) => {
 					console.log(uploadFileErr);
-						uni.showToast({
-							title: '上传失败',
-							icon: 'none'
+						uni.showModal({
+							content: '上传失败',
+							showCancel:false,
+							confirmColor:"#000000"
 						});
+						uni.hideLoading();
 					}
 				});
 			},
 			fail: res=>{
-				uni.showToast({
+				uni.showModal({
 					title: '请先选择图片',
 					icon: 'none'
 				});
@@ -109,6 +118,26 @@ swiper-item{
 	font-size: 16px;
 	color: #878787;
 	text-align: center;
+}
+
+.predict-result{
+	display:flex;
+	align-items: center;
+	justify-content: center;
+	margin-bottom: 5vh;
+}
+.predict-result{
+	font-size:16px;
+}
+.result-btn{
+	width:76px;
+	height:22px;
+	font-size:8px;
+	line-height:22px;
+	left: 50px;
+	background-color: #A4ADB3;
+	border-color:#BBBBBB;
+	color:white;
 }
 
 .predict-btn{
