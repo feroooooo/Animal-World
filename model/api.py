77d4e90058554215bef8e83fa272a10cf348ee5f
feroooooo <1799363617@qq.com>
2024-03-06 15:40:14 +0800

    crawl

--- model/api.py
+++ model/api.py
@@ -9,7 +9,8 @@ def predict(url):
     f = open(url, 'rb')
     img = base64.b64encode(f.read())
 
-    params = {"image":img, 'baike_num':1}
+    # params = {"image":img, 'baike_num':1}
+    params = {"image":img}
     access_token = '24.4b0a8ec9a214ed6e8d0621fc3a614566.2592000.1712219879.282335-55261071'
     request_url = request_url + "?access_token=" + access_token
     headers = {'content-type': 'application/x-www-form-urlencoded'}
@@ -17,34 +18,35 @@ def predict(url):
     if response:
         ret = {}
         response_dict = response.json()
-        print(response_dict)
+        # print(response_dict)
         ret['prediction'] = response_dict['result'][0]['name']
         if ret['prediction'] == '非动物':
             ret['is_animal'] = False
         else:
             ret['is_animal'] = True
-        try:
-            ret['image_url'] = response_dict['result'][0]['baike_info']['image_url']
-            ret['baike_url'] = response_dict['result'][0]['baike_info']['baike_url']
-            ret['description'] = response_dict['result'][0]['baike_info']['description']
+        ret['genus'] = crawl_wiki_data(ret['prediction'])
+        # try:
+        #     ret['image_url'] = response_dict['result'][0]['baike_info']['image_url']
+        #     ret['baike_url'] = response_dict['result'][0]['baike_info']['baike_url']
+        #     ret['description'] = response_dict['result'][0]['baike_info']['description']
 
-            # 删除空格
-            no_spaces_string = ret['description'].replace(" ", "")
-            # 查找'属'字符的位置
-            pos_shu = no_spaces_string.find('属')
-            # 在'属'字符之前查找'科'或'、'字符的位置
-            pos_ke = no_spaces_string.rfind('科', 0, pos_shu)
-            pos_dou = no_spaces_string.rfind('、', 0, pos_shu)
-            # 使用两个位置中较大的那一个作为起始位置
-            start_pos = max(pos_ke, pos_dou) + 1
-            # 提取子字符串
-            ret['genus'] = no_spaces_string[start_pos:pos_shu]
+        #     # 删除空格
+        #     no_spaces_string = ret['description'].replace(" ", "")
+        #     # 查找'属'字符的位置
+        #     pos_shu = no_spaces_string.find('属')
+        #     # 在'属'字符之前查找'科'或'、'字符的位置
+        #     pos_ke = no_spaces_string.rfind('科', 0, pos_shu)
+        #     pos_dou = no_spaces_string.rfind('、', 0, pos_shu)
+        #     # 使用两个位置中较大的那一个作为起始位置
+        #     start_pos = max(pos_ke, pos_dou) + 1
+        #     # 提取子字符串
+        #     ret['genus'] = no_spaces_string[start_pos:pos_shu]
         
-        except:
-            ret['image_url'] = ''
-            ret['baike_url'] = ''
-            ret['description'] = ''
-            ret['genus'] = ''
+        # except:
+        #     ret['image_url'] = ''
+        #     ret['baike_url'] = ''
+        #     ret['description'] = ''
+        #     ret['genus'] = ''
         return ret
     else:
         return {}
@@ -73,11 +75,14 @@ def crawl_wiki_data(animal):
     url='https://baike.baidu.com/item/' + animal      
     try:
         response = requests.get(url,headers=headers)
-        print(response.text)
-         
+        pos = response.text.find(r'属</dt>')
+        sub_string = response.text[pos+1:pos+300]
+        return sub_string[sub_string.find('/item/') + 6:sub_string.find('属')]
     except Exception as e:
         print(e)
+        return ''
 
 if __name__=="__main__":
     # get_access_token()
-    print(predict(r'C:\Custom\DataSet\WildLife\fox\00000035_224resized.jpg'))   
+    prediction = predict(r'C:\Custom\DataSet\WildLife\fox\00000035_224resized.jpg')
+    print(prediction)
